6631dc2f25ad380ca82291713b62320769cfcc8e
Vladimir Dulenov <originalblack@mail.ru>
2023-01-07 22:41:15 +0100

    Pkg controller updated. Tmpl funcs added

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,10 +1,15 @@
 package controller
 
-
 import (
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/client-go/kubernetes"
+	"context"
+	"log"
 
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 )
 
 func ignoreNotFound(err error) error {
@@ -15,6 +20,37 @@ func ignoreNotFound(err error) error {
 }
 
 type NSCleaner struct {
-	DryRun bool
-	kclient     *kubernetes.Clientset
+	kclient    *kubernetes.Clientset
+	nsInformer cache.SharedIndexInformer // should i use cache for NS ?????
+
+	dryRun bool
+	ctx    context.Context
+	stopCh <-chan struct{}
+}
+
+func NewNSCleaner(ctx context.Context, kclient *kubernetes.Clientset) *NSCleaner {
+	return &NSCleaner{
+		ctx:     ctx,
+		kclient: kclient,
+	}
+}
+
+func (c *NSCleaner) Run() {
+
+}
+
+func (c *NSCleaner) DeleteNamespace() {
+
+}
+
+func (c *NSCleaner) GetNamepsaces() (*v1.NamespaceList, error) {
+
+	nsList, err := c.kclient.CoreV1().Namespaces().List(c.ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return nsList, nil
+
 }
