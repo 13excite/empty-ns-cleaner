de57346c77165512bbe04599ae8b790daaa3a875
Vladimir Dulenov <originalblack@mail.ru>
2023-01-08 22:13:49 +0100

    Controller updated
    New methods added. Now it's a MVP state.
    Will be refactored in the future

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -7,6 +7,7 @@ import (
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
@@ -50,7 +51,46 @@ func (c *NSCleaner) GetNamepsaces() (*v1.NamespaceList, error) {
 		log.Fatal(err)
 		return nil, err
 	}
-
 	return nsList, nil
+}
 
+// UpdateLabels updates given namespace
+// Should use only for updating labels
+// but also can be use for updating any fields
+func (c *NSCleaner) UpdateLabels(obj *v1.Namespace) error {
+	_, err := c.kclient.CoreV1().Namespaces().Update(c.ctx, obj, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateLablels uses official k8s way
+// for updating labels
+// todo state
+func (c *NSCleaner) UpdateLablelsDISABLED() {
+	// working with labels
+	labels := map[string]string{
+		"testlabel":  "value",
+		"testlabel1": "value",
+	}
+	// shoud be pointer to obj
+	//
+	accessor, err := meta.Accessor(&struct{}{})
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	objLabels := accessor.GetLabels()
+	if objLabels == nil {
+		objLabels = make(map[string]string)
+	}
+
+	for key, value := range labels {
+		objLabels[key] = value
+	}
+	//fmt.Println(n)
+
+	accessor.SetLabels(objLabels)
+	// end labels blocks
 }
