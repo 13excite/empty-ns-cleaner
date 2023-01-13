83ca39deadf768e38000b1714bd02f4ef99d62fb
Vladimir Dulenov <originalblack@mail.ru>
2023-01-13 16:30:03 +0100

    Commets were added inside pkg/utils/

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,6 +4,9 @@ import (
 	"sort"
 )
 
+// IsProtectedNs returns true is namespace exists in
+// protected namespaces list. BinarySearch uses here
+// because a cluster can containe a couple hundred namespaces
 func IsProtectedNs(protectedNS []string, namespaceName string) bool {
 	i := sort.Search(len(protectedNS), func(i int) bool { return protectedNS[i] >= namespaceName })
 
@@ -14,9 +17,12 @@ func IsProtectedNs(protectedNS []string, namespaceName string) bool {
 	}
 }
 
-func Contains(s []string, e string) bool {
+// ContainsVerb returns true if string exists in slice
+// Simple iterations uses here, because because
+// verbSlice contains as usual less then 10 items
+func ContainsVerb(s []string, v string) bool {
 	for _, a := range s {
-		if a == e {
+		if a == v {
 			return true
 		}
 	}
