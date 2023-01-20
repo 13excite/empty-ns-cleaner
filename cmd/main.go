a977941bf0fc6476824376c039485e96a447f5af
Vladimir Dulenov <originalblack@mail.ru>
2023-01-20 21:00:50 +0100

    Simple main.go added

--- cmd/main.go
+++ cmd/main.go
@@ -3,101 +3,41 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/13excite/empty-ns-cleaner/pkg/config"
 	"github.com/13excite/empty-ns-cleaner/pkg/controller"
+	"github.com/13excite/empty-ns-cleaner/pkg/kube"
+
 	//"github.com/13excite/empty-ns-cleaner/pkg/utils"
 	//"k8s.io/apimachinery/pkg/api/errors"
 	//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	"log"
-	"os"
-	"path/filepath"
 )
 
 func main() {
 
-	// don't mark this NS
+	config.C.Defaults()
 
 	fmt.Println("RUN")
-	clientset, err := newClientSet(true)
+	clientset, err := kube.NewClientSet(true)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	discClient, err := newDiscoveryClient(true)
+	discClient, err := kube.NewDiscoveryClient(true)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	dynClient, err := newDynamicClient(true)
+	dynClient, err := kube.NewDynamicClient(true)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	ctx := context.Background()
-	kContrl := controller.NewNSCleaner(ctx, clientset, discClient, dynClient)
+	kContrl := controller.NewNSCleaner(ctx, &config.C,
+		clientset, discClient, dynClient,
+	)
 
 	kContrl.Run(ctx)
 
-}
-
-func newClientSet(outsideCluster bool) (*kubernetes.Clientset, error) {
-	kubeConfigPath := ""
-	if outsideCluster {
-		kubeConfigPath = filepath.Join(os.Getenv("KUBECONFIG"))
-		fmt.Println(kubeConfigPath)
-	}
-	// if all args of BuildConfigFromFlags is ""
-	// then rest.InClusterConfig() will be activeted
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(config)
-
-	return kubernetes.NewForConfig(config)
-}
-
-func newDynamicClient(outsideCluster bool) (*dynamic.DynamicClient, error) {
-	kubeConfigPath := ""
-	if outsideCluster {
-		kubeConfigPath = filepath.Join(os.Getenv("KUBECONFIG"))
-		fmt.Println(kubeConfigPath)
-	}
-	// if all args of BuildConfigFromFlags is ""
-	// then rest.InClusterConfig() will be activeted
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		return nil, err
-	}
-
-	dynClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return dynClient, nil
-}
-
-func newDiscoveryClient(outsideCluster bool) (*discovery.DiscoveryClient, error) {
-	kubeConfigPath := ""
-	if outsideCluster {
-		kubeConfigPath = filepath.Join(os.Getenv("KUBECONFIG"))
-		fmt.Println(kubeConfigPath)
-	}
-	// if all args of BuildConfigFromFlags is ""
-	// then rest.InClusterConfig() will be activeted
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		return nil, err
-	}
-
-	dycoveryClient, err := discovery.NewDiscoveryClientForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return dycoveryClient, nil
 }
