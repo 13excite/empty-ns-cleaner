b820c93a570c943cfd10f84cafcb034ed62ed18a
Vladimir Dulenov <originalblack@mail.ru>
2023-01-23 21:38:48 +0100

    annotation_test.go added

---
+++ pkg/controller/annotation_test.go
@@ -0,0 +1,112 @@
+package controller
+
+import (
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddWillRemoveAnnotation(t *testing.T) {
+	testCases := []struct {
+		name                     string
+		namespaces               []runtime.Object
+		nsName                   string
+		targetAnnotationsKey     string
+		expectedAnnotationsValue string
+		expectedError            error
+	}{
+		{
+			name:                     "adding will-removed annotations for existing ns",
+			nsName:                   "my-namespace",
+			targetAnnotationsKey:     "remove-empty-ns-operator/will-removed",
+			expectedAnnotationsValue: "True",
+			expectedError:            nil,
+			namespaces: []runtime.Object{
+				&v1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "my-namespace",
+						Labels: map[string]string{
+							"label1": "value1",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		fakeClientset := fake.NewSimpleClientset(tc.namespaces...)
+		ctrl := &NSCleaner{
+			clientSet: fakeClientset,
+		}
+
+		err := ctrl.AddWillRemoveAnnotation(tc.nsName)
+		require.Equal(t, tc.expectedError, err, tc.name)
+
+		got, err := ctrl.GetNamepsaces()
+		nsList := got.Items
+
+		require.Equal(t, tc.expectedError, err, tc.name+". Getting namespaces")
+
+		require.Equal(t, nsList[0].ObjectMeta.Annotations[tc.targetAnnotationsKey],
+			tc.expectedAnnotationsValue, tc.name,
+		)
+
+	}
+}
+
+func TestDeleteRemoveAnnotation(t *testing.T) {
+	testCases := []struct {
+		name                     string
+		namespaces               []runtime.Object
+		nsName                   string
+		targetAnnotationsKey     string
+		expectedAnnotationsValue string
+		expectedError            error
+	}{
+		{
+			name:                     "deleting will-removed annotations for existing ns",
+			nsName:                   "my-namespace",
+			targetAnnotationsKey:     "remove-empty-ns-operator/will-removed",
+			expectedAnnotationsValue: "False",
+			expectedError:            nil,
+			namespaces: []runtime.Object{
+				&v1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "my-namespace",
+						Labels: map[string]string{
+							"label1": "value1",
+						},
+						Annotations: map[string]string{
+							"remove-empty-ns-operator/will-removed": "True",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		fakeClientset := fake.NewSimpleClientset(tc.namespaces...)
+		ctrl := &NSCleaner{
+			clientSet: fakeClientset,
+		}
+
+		err := ctrl.DeleteWillRemoveAnnotation(tc.nsName)
+		require.Equal(t, tc.expectedError, err, tc.name)
+
+		got, err := ctrl.GetNamepsaces()
+		nsList := got.Items
+
+		require.Equal(t, tc.expectedError, err, tc.name+". Getting namespaces")
+
+		require.Equal(t, nsList[0].ObjectMeta.Annotations[tc.targetAnnotationsKey],
+			tc.expectedAnnotationsValue, tc.name,
+		)
+	}
+}
